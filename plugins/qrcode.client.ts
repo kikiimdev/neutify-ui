ecc5067e52e88aa7d76dc8d06592702a579968b1
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2025-01-09 04:42:30 +0800

    fix: import * on qr-code-styling

--- plugins/qrcode.client.ts
+++ plugins/qrcode.client.ts
@@ -1,13 +1,14 @@
-import _QRCodeStyling, { type Options } from "qr-code-styling"
+import * as _QRCodeStyling from "qr-code-styling"
+import type { Options } from "qr-code-styling"
 
-const QRCodeStyling = ((_QRCodeStyling as any)?.default as typeof _QRCodeStyling) ?? _QRCodeStyling
+const QRCodeStyling = ((_QRCodeStyling as any)?.default as any) ?? _QRCodeStyling
 
 export default defineNuxtPlugin((nuxtApp) => {
-  let qrCodeStyling: _QRCodeStyling
+  let qrCodeStyling: any
   return {
     provide: {
       // Provide a helper that returns an instance of QRCodeStyling
-      qrCodeStyling: (options: Partial<Options>): _QRCodeStyling => {
+      qrCodeStyling: (options: Partial<Options>) => {
         if (qrCodeStyling) return qrCodeStyling
         qrCodeStyling = new QRCodeStyling(options)
         return qrCodeStyling
